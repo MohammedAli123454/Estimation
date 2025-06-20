626134e6730d67388da1f65f492f7b8ac0086c87
MohammedAli123454 <azcsmohdali1@gmail.com>
2025-06-20 21:34:09 +0300

    dfdg

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,13 @@
-import { defineConfig } from 'drizzle-kit';
 
-export default defineConfig({
+
+export default {
   dialect: 'postgresql',
   schema: "/Users/mohammwdali/Documents/Next JS Projects/dashboard/app/config/schema.ts",
   out: "./drizzle",
-
   dbCredentials: {
     url: "postgresql://azcsmohdali1:f1NauxJ0RikY@ep-morning-snowflake-15132543-pooler.ap-southeast-1.aws.neon.tech/domus-dashboard?sslmode=require"
-  },
-
-});
+  }
+};
 
 
 
